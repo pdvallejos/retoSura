046cb6effe32ef819375dbb85d146f3a7b470db4
Pablo Vallejos <pdvallejos@gmail.com>
2023-02-23 12:51:03 -0300

    se agrega setup

--- co.com.retoSura/src/main/java/co/com/retoSura/enums/Diccionario.java
+++ co.com.retoSura/src/main/java/co/com/retoSura/enums/Diccionario.java
@@ -5,6 +5,6 @@ public class Diccionario {
     private Diccionario() {}
 
     public static final String URL_BASE_GUI = "https://magento.softwaretestingboard.com/";
-
     public static final String ACTOR_NAME = "Fabricio";
+    public static final String RUTA_DRIVERS = "src/test/resources/drivers";
 }
